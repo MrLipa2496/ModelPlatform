6a3873ef21e32c6852f20687a1d95c632f2330be
MrLipa2496 <sasalipa216@gmail.com>
2025-10-04 14:47:41 +0300

    Add cors in server

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = require('./routers');
+const cors = require('cors');
 
 const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
