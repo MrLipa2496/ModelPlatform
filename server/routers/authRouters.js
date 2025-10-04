6f16b0065ba86f949ad7b001250c5fbd884d385c
MrLipa2496 <sasalipa216@gmail.com>
2025-10-04 13:10:19 +0300

    Add auth routes

--- server/routers/authRouters.js
+++ server/routers/authRouters.js
@@ -3,8 +3,10 @@ const authController = require('../controllers/authController');
 
 const authRouter = Router();
 
-authRouter.post('/registration', authController.registration);
+authRouter.post('/signup/model', authController.signupModel);
+authRouter.post('/signup/agency', authController.signupAgency);
+
 authRouter.post('/login', authController.login);
-authRouter.get('/users', authController.getUser);
+authRouter.get('/logout', authController.logout);
 
 module.exports = authRouter;
