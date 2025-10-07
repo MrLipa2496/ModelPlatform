066d825e9ac9931d13ddce7708fd442b7a6123b7
MrLipa2496 <sasalipa216@gmail.com>
2025-10-07 13:22:16 +0300

    Upgrade structure of Routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,12 @@ import './reset.css';
 import BasePage from './pages/BasePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import HomePage from './pages/GuestPages/HomePage';
+import ModelsPage from './pages/GuestPages/ModelsPage';
+import CastingsPage from './pages/GuestPages/CastingsPage';
+import AboutPage from './pages/GuestPages/AboutPage';
+import ContactsPage from './pages/GuestPages/ContactsPage';
+import RoleRoute from './components/RoleRoute';
 
 function App () {
   return (
@@ -10,7 +16,14 @@ function App () {
       <Routes>
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
-        <Route path='/' element={<BasePage />} />
+
+        <Route path='/' element={<BasePage />}>
+          <Route index element={<HomePage />} />
+          <Route path='models' element={<ModelsPage />} />
+          <Route path='castings' element={<CastingsPage />} />
+          <Route path='about' element={<AboutPage />} />
+          <Route path='contacts' element={<ContactsPage />} />
+        </Route>
       </Routes>
     </Router>
   );
