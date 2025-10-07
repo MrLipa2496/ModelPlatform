1056477ba512e1cb0cd0b2bef065fb06a9fdaed6
MrLipa2496 <sasalipa216@gmail.com>
2025-10-07 14:01:01 +0300

    Add structure of OfferPage & CastingPage & DashboardPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,11 @@ import './reset.css';
 import BasePage from './pages/BasePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import RoleRoute from './components/RoleRoute/RoleRoute';
+import DashboardPage from './pages/ModelPages/DashboardPage';
+import OfferPage from './pages/ModelPages/OfferPage';
+import CastingPage from './pages/ModelPages/CastingPage';
+import ProfilePage from './pages/ModelPages/ProfilePage';
 
 function App () {
   return (
@@ -10,7 +15,15 @@ function App () {
       <Routes>
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
-        <Route path='/' element={<BasePage />} />
+
+        <Route path='/' element={<BasePage />}>
+          <Route element={<RoleRoute allowedRoles={['model']} />}>
+            <Route path='/dashboard' element={<DashboardPage />} />
+            <Route path='/offers' element={<OfferPage />} />
+            <Route path='/castings' element={<CastingPage />} />
+            <Route path='/profile' element={<ProfilePage />} />
+          </Route>
+        </Route>
       </Routes>
     </Router>
   );
---
+++ client/src/pages/ModelPages/CastingPage/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CastingPage () {
+  return <div>CastingPage</div>;
+}
