055ac817bd9e513043f262e153bdc56ffb005a6d
MrLipa2496 <sasalipa216@gmail.com>
2025-10-08 19:25:27 +0300

    Add endpoint model in index

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const authRouter = require('./authRouters');
+const modelRouter = require('./modelRouters');
 
 const router = Router();
 
 router.use('/auth', authRouter);
+router.use('/model', modelRouter);
 
 module.exports = router;
