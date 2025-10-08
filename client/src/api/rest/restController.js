5a6a0c7a62fbc32dbdfc0c58cd09fc959e66a752
MrLipa2496 <sasalipa216@gmail.com>
2025-10-08 19:26:45 +0300

    Add model restController

--- client/src/api/rest/restController.js
+++ client/src/api/rest/restController.js
@@ -1,9 +1,18 @@
 import http from '../interceptor';
 
+// AUTH
 export const signupModelRequest = data =>
   http.post('api/auth/signup/model', data);
 export const signupAgencyRequest = data =>
   http.post('api/auth/signup/agency', data);
-
 export const loginRequest = data => http.post('api/auth/login', data);
 export const logoutRequest = () => http.get('api/auth/logout');
+
+// MODELS
+export const getModelProfileRequest = () => http.get('api/model/profile');
+export const updateModelProfileRequest = data =>
+  http.put('api/model/profile', data);
+export const updateModelPhotoRequest = formData =>
+  http.patch('api/model/profile/photo', formData, {
+    headers: { 'Content-Type': 'multipart/form-data' },
+  });
