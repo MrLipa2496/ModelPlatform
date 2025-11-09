06b14c790b2e3d522c41b7c1f08c4ff43e233e90
llydom <tsipa2345@gmail.com>
2025-11-09 18:05:44 +0200

    Add albumSlice in store

--- client/src/store/reducer.js
+++ client/src/store/reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import authReducer from './slices/authSlice';
 import modelReducer from './slices/modelSlice';
 import agencyReducer from './slices/agencySlice';
+import albumReducer from './slices/albumSlice';
 
 const rootReducer = combineReducers({
   agency: agencyReducer,
   auth: authReducer,
   model: modelReducer,
+  album: albumReducer,
 });
 
 export default rootReducer;
