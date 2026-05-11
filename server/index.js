77f1d3fc9b0bdbbb993ee84569843b8fc941d55d
MrLipa2496 <sasalipa216@gmail.com>
2026-05-11 10:23:43 +0200

    feat: add database synchronization on startup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const http = require('node:http');
 require('dotenv').config();
 const app = require('./app');
+const db = require('./models');
 
 const PORT = process.env.PORT || 5001;
 
 const httpServer = http.createServer(app);
 
-httpServer.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+db.sequelize
+  .sync({ alter: true })
+  .then(() => {
+    console.log('Database connected and synced');
+
+    httpServer.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Unable to sync database:', err);
+  });
