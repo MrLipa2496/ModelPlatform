7926ec748ec3f09709aca1d6250eeb634846e3d0
MrLipa2496 <sasalipa216@gmail.com>
2026-05-11 09:08:44 +0200

    fix: update database config for production postgres and SSL

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST ?? '127.0.0.1',
     dialect: 'postgres',
-    port: Number(process.env.DB_PORT) ?? 5000,
+    port: Number(process.env.DB_PORT) || 5432,
     seederStorage: 'sequelize',
   },
   test: {
@@ -16,10 +16,18 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: Number(process.env.DB_PORT) || 5432,
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
