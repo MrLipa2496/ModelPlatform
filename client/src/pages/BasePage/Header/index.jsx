95a8d8dfadad7d4d4183266e0dd14ea09c78f8c6
MrLipa2496 <sasalipa216@gmail.com>
2026-06-04 10:14:08 +0200

    refactor(landing): upgrade header

--- client/src/pages/BasePage/Header/index.jsx
+++ client/src/pages/BasePage/Header/index.jsx
@@ -1,7 +1,7 @@
-import { useState, useMemo } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { useState, useMemo, useEffect } from 'react';
+import { NavLink, useNavigate, useLocation } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { CgProfile } from 'react-icons/cg';
+import { CgProfile, CgClose } from 'react-icons/cg';
 import { useSelector, useDispatch } from 'react-redux';
 import classNames from 'classnames';
 import { logout } from '../../../store/slices/authSlice';
@@ -11,11 +11,24 @@ import styles from './Header.module.sass';
 function Header () {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
   const dispatch = useDispatch();
 
-  const { user, isFetching } = useSelector(state => state.auth);
+  const { user } = useSelector(state => state.auth);
 
   const toggleMenu = () => setIsMenuOpen(prev => !prev);
+
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [location.pathname]);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  }, [isMenuOpen]);
 
   const handleLogout = () => {
     dispatch(logout());
@@ -42,62 +55,68 @@ function Header () {
 
   return (
     <header className={styles.headerWrapper}>
-      <NavLink className={styles.logoNavLink} to='/'>
-        <div className={styles.headerLogoWrapper}>
-          <p className={styles.headerLogo}>
-            Lipa<span className={styles.headerAbbreviation}>X</span>
-          </p>
-        </div>
-      </NavLink>
+      <div className={styles.innerContainer}>
+        <NavLink className={styles.logoNavLink} to='/'>
+          <div className={styles.headerLogoWrapper}>
+            <p className={styles.headerLogo}>
+              Lipa<span className={styles.headerAbbreviation}>X</span>
+            </p>
+          </div>
+        </NavLink>
 
-      <button className={styles.menuBtn} onClick={toggleMenu} aria-label='Menu'>
-        <GiHamburgerMenu />
-      </button>
+        <button
+          className={styles.menuBtn}
+          onClick={toggleMenu}
+          aria-label='Menu'
+        >
+          {isMenuOpen ? <CgClose /> : <GiHamburgerMenu />}
+        </button>
 
-      <nav
-        className={classNames(styles.headerNav, {
-          [styles.menuOpen]: isMenuOpen,
-        })}
-      >
-        {regularLinks.map(({ to, label, className }) => (
-          <li key={to} className={styles.navLi}>
-            <NavLink
-              className={props => navLinkClassName(props, className)}
-              to={to}
-            >
-              {label === 'Profile' ? (
-                <CgProfile className={styles.profileIcon} />
-              ) : (
-                label
-              )}
-            </NavLink>
-          </li>
-        ))}
-
-        {currentRole === 'guest' && authLinks.length > 0 && (
-          <li className={classNames(styles.navLi, styles.authNavLi)}>
-            <div className={styles.authButtonsWrapper}>
-              {authLinks.map(({ to, label, className }) => (
+        <nav
+          className={classNames(styles.headerNav, {
+            [styles.menuOpen]: isMenuOpen,
+          })}
+        >
+          <ul className={styles.navList}>
+            {regularLinks.map(({ to, label, className }) => (
+              <li key={to} className={styles.navLi}>
                 <NavLink
-                  key={to}
                   className={props => navLinkClassName(props, className)}
                   to={to}
                 >
-                  {label}
+                  {label === 'Profile' ? (
+                    <CgProfile className={styles.profileIcon} />
+                  ) : (
+                    label
+                  )}
                 </NavLink>
-              ))}
-            </div>
-          </li>
-        )}
+              </li>
+            ))}
 
-        {user && (
-          <li className={styles.navLi}>
-            <button onClick={handleLogout} className={styles.logoutBtn}>
-              Logout
-            </button>
-          </li>
-        )}
-      </nav>
+            {currentRole === 'guest' && authLinks.length > 0 && (
+              <li className={classNames(styles.navLi, styles.authGroup)}>
+                {authLinks.map(({ to, label, className }) => (
+                  <NavLink
+                    key={to}
+                    className={props => navLinkClassName(props, className)}
+                    to={to}
+                  >
+                    {label}
+                  </NavLink>
+                ))}
+              </li>
+            )}
+
+            {user && (
+              <li className={styles.navLi}>
+                <button onClick={handleLogout} className={styles.logoutBtn}>
+                  Logout
+                </button>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
